c74da557f4cf49a7ba837863579b615de5d3d8af
Carlos-Ballon <carloebs@gmail.com>
2024-06-12 02:40:06 -0500

    Updated

--- scripts/histograms and density plots.R
+++ scripts/histograms and density plots.R
@@ -6,27 +6,28 @@ shapiro_p <- function(x) {
 }
 
 # Function to create histograms and density plots for each variable in a data frame
-total_plots <- function(df) {
+total_plots <- function(.data) {
   
   # Create function components
-  plots <- lapply(names(df), function(var) {
-    # calculate the p-value of the Shapiro-Wilk test for each variable
-    p_value <- shapiro_p(df[[var]])
+  plots <- lapply(names(.data), function(var) {
+    # Calculate the p-value of the Shapiro-Wilk test for each variable
+    p_value <- shapiro_p(.data[[var]])
     
     # Summary statistics for each variable
-    mean_value <- mean(df[[var]])
-    median_value <- median(df[[var]])
-    q1_value <- quantile(df[[var]], probs = 0.25) 
-    q3_value <- quantile(df[[var]], probs = 0.75)
+    mean_value <- mean(.data[[var]], na.rm = TRUE)
+    median_value <- median(.data[[var]], na.rm = TRUE)
+    q1_value <- quantile(.data[[var]], probs = 0.25, na.rm = TRUE) 
+    q3_value <- quantile(.data[[var]], probs = 0.75, na.rm = TRUE)
     
     # Histogram and density plot for each variable
-    plot <- ggplot(data = df, aes_string(x = var)) +
-      geom_histogram(aes(y = after_stat(density)), bins = 10, 
-                     fill = "#3B3B3BFF", color = "white", alpha = 0.8) +
-      geom_density(fill = "#0073C2FF", col = "#00B5E2FF", alpha = 0.3, linewidth = 1) +
-      geom_vline(aes(xintercept = mean(df[[var]])), 
+    plot <- ggplot(data = .data, aes_string(x = var)) +
+      geom_histogram(aes(y = after_stat(density)), bins = 10, alpha = 0.8,
+                     fill = "#3B3B3BFF", color = "white", na.rm = TRUE) +
+      geom_density(fill = "#0073C2FF", col = "#00B5E2FF", 
+                   alpha = 0.3, linewidth = 1, na.rm = TRUE) +
+      geom_vline(aes(xintercept = mean(.data[[var]], na.rm = TRUE)), 
                  color = "#D51317FF", linetype = "dashed", linewidth = 1) +
-      geom_vline(aes(xintercept = median(df[[var]])), 
+      geom_vline(aes(xintercept = median(.data[[var]], na.rm = TRUE)), 
                  color = "#95C11FFF", linetype = "dashed", linewidth = 1) +
       ggtitle(paste("Histogram and Density Plot of\n", var)) +
       labs(x = element_blank()) +
@@ -40,7 +41,7 @@ total_plots <- function(df) {
     
     # Add the p-value and summary statistics for each variable to the plot
     plot <- plot + 
-      annotate("text", x = Inf, y = 0, hjust = 1.20, vjust = 1,
+      annotate("text", x = Inf, y = 0, hjust = 1, vjust = 1.2,
                label = paste("Shapiro-Wilk test p-value =", p_value), 
                size = 3.3) +
       annotate("text", x = Inf, y = Inf, hjust = 1, vjust = 1,
